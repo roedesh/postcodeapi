7948b26c42514d6b5e56d1df5ba4a64a07761880
Ruud Schroën <schroenruud@gmail.com>
2018-11-23 00:10:07 +0100

    Updated requests dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="postcodeapi",
-    version="1.0.0",
+    version="1.0.1",
     description="A tiny wrapper around the Postcode API v2",
     long_description=open("README.md").read(),
     url="http://github.com/roedesh/postcodeapi",
